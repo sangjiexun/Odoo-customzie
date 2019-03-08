317f29e24850e4629c734e11ce3fd11c9d7e74b5
Youshinan <youshinan82@gmail.com>
2019-03-08 22:48:00 +0900

    新規作成

---
+++ customzie-addons/production_line/models/product.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    product_no = fields.Char('Product No', compute='_compute_product_no')
+
+    @api.one
+    def _compute_product_no(self):
+        self.code = "123456"
