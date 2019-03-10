66d2d6190a8e295fe17546a35791fda76d168740
Youshinan <youshinan82@gmail.com>
2019-03-10 09:04:39 +0900

    modify the after service

--- customzie-addons/after_service/__manifest__.py
+++ customzie-addons/after_service/__manifest__.py
@@ -4,6 +4,7 @@
     'description': "After service",
     'version': '12.0.1.0',
     'category': '',
+    'author': "youshinan",
     'depends': ['base', 'sale', 'stock', 'production_line'],
     'application': True,
     'license': 'LGPL-3',
@@ -11,6 +12,9 @@
         'views/after_service_views.xml',
         'views/after_service_templates.xml',
         'data/treatment_data.xml',
+        'data/ir_sequence_data.xml',
+        'report/after_service_reports.xml',
+        'report/after_service_templates.xml',
         'security/after_service_security.xml',
         'security/ir.model.access.csv',
 
--- customzie-addons/after_service/models/after_service.py
+++ customzie-addons/after_service/models/after_service.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError, ValidationError
 from odoo.exceptions import Warning
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    after_service_id = fields.Many2one('after.service')
 
 
 class AfterService(models.Model):
@@ -9,7 +16,11 @@ class AfterService(models.Model):
     _description = 'After Service'
     _order = 'id'
 
-    name = fields.Char('After Service', required=True, index=True)
+    name = fields.Char(
+        'After Service',
+        default=lambda self: self.env['ir.sequence'].next_by_code('after.service'),
+        copy=False, required=True, readonly=True)
+
     # Sale
     # production_line_id = fields.Many2one('production.line', string='Production Reference', ondelete='cascade', index=True, copy=False, readonly=True)
     order_id = fields.Char('Sale Order', index=True, readony=True)
@@ -21,6 +32,46 @@ class AfterService(models.Model):
     treatment_book_id = fields.Many2one(
         "treatment.book", string="Treatment Type", required=True)
     treatment_remark = fields.Text(string='Treatment Remark')
+    date_approve = fields.Date('Approval Date', readonly=True, index=True, copy=False)
+    state = fields.Selection([
+        ('draft', 'Quotation'),
+        ('to approve', 'To Approve'),
+        ('done', 'Locked'),
+        ('cancel', 'Cancelled')], string='Status',
+        copy=False, default='draft', readonly=True, track_visibility='onchange')
+    move_id = fields.Many2one(
+        'stock.move', 'Move',
+        copy=False, readonly=True, track_visibility="onchange",
+        help="Move created by the After service")
+
+    @api.onchange('treatment_book_id')
+    def onchange_treatment_book_id(self):
+        if self.treatment_book_id:
+            self.treatment_remark = self.treatment_book_id.treatment_book
+
+    @api.multi
+    def print_after_service(self):
+        return self.env.ref('after_service.action_report_after_service').report_action(self)
+
+    @api.multi
+    def button_approve(self, force=False):
+        self.write({'state': 'done', 'date_approve': fields.Date.context_today(self)})
+        return {}
+
+    @api.multi
+    def button_draft(self):
+        self.write({'state': 'draft'})
+        return {}
+
+    @api.multi
+    def button_confirm(self):
+        self.write({'state': 'to approve'})
+        return {}
+
+    @api.multi
+    def button_cancel(self):
+        self.write({'state': 'cancel'})
+        return {}
 
 
 class TreatmentBook(models.Model):
@@ -28,6 +79,6 @@ class TreatmentBook(models.Model):
     _description = 'Treatment Book'
     _order = 'id'
 
-    name = fields.Char('Treatment', required=True, index=True)
+    name = fields.Char('Treatment', required=True, index=True, translate=True)
     treatment_book = fields.Text(string='Treatment Book')
 
