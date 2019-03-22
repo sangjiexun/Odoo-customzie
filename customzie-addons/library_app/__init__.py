fd4744c4d83260590428ecc6d7b29bf199e71348
Youshinan <youshinan82@gmail.com>
2019-03-22 20:34:30 +0900

    modify the after service

--- customzie-addons/library_app/__init__.py
+++ customzie-addons/library_app/__init__.py
@@ -1,6 +1,4 @@
 from . import models
-
-from . import models
 from . import controllers
 
 
--- customzie-addons/library_app/models/library_book.py
+++ customzie-addons/library_app/models/library_book.py
@@ -2,18 +2,75 @@
 
 from odoo import api, fields, models
 from odoo.exceptions import Warning
+from odoo.exceptions import ValidationError
+
 
 class Book(models.Model):
     _name = 'library.book'
     _description = 'Book'
     _order = 'name, date_published desc'
+
+    # string fields
     name = fields.Char('Title', required= True )
     isbn = fields.Char('ISBN')
-    active = fields.Boolean('Active?', default= True )
+    book_type = fields.Selection(
+        [('paper', 'Paperback'),
+         ('hard', 'Hardcover'),
+         ('electronic', 'Electronic'),
+         ('other', 'Other')],
+        'Type')
+    notes = fields.Text('Internal Notes')
+    descr = fields.Html('Description')
+
+    # Numeric fields:
+    copies = fields.Integer(default=1)
+    avg_rating = fields.Float('Average Rating', (3, 2))
+    price = fields.Monetary('Price', 'currency_id')
+    currency_id = fields.Many2one('res.currency')  # price helper
+
+    # Date and time fields
     date_published = fields.Date()
+    last_borrow_date = fields.Datetime(
+        'Last Borrowed On',
+        default=lambda self: fields.Datetime.now())
+    # Other fields
+    active = fields.Boolean('Active?', default=True)
     image = fields.Binary('Cover')
+
     publisher_id = fields.Many2one('res.partner', string='Publisher')
     author_ids = fields.Many2many('res.partner', string='Authors')
+
+    # 添加图书分类
+    category_id = fields.Many2one('library.book.category', string='Category')
+    # 添加图书国别
+    publisher_country_id = fields.Many2one(
+        'res.country', string='Publisher Country',
+        compute='_compute_publisher_country',
+        inverse='_inverse_publisher_country',
+        search='_search_publisher_country',
+    )
+
+    _sql_constraints = [(
+        'library_book_name_date_uq',  # 约束唯一标识符
+        'UNIQUE (name, date_published)',  # 约束 SQL 语法
+        'Book title and publication date must be unique'),  # 消息
+        ('library_book_check_date',
+         'CHECK (date_published <= current_date)',
+         'Publication date must not be in the future.'),
+    ]
+
+    # 计算图书国别依赖
+    @api.depends('publisher_id.country_id')
+    def _compute_publisher_country(self):
+        for book in self:
+            book.publisher_country_id = book.publisher_id.country_id
+
+    def _inverse_publisher_country(self):
+        for book in self:
+            book.publisher_id.country_id = book.publisher_country_id
+
+    def _search_publisher_country(self, opearator, value):
+        return [('publisher_id.country_id', operator, value)]
 
     @api.multi
     def _check_isbn(self):
@@ -27,7 +84,6 @@ class Book(models.Model):
             check = 10 - remain if remain != 0 else 0
             return digits[-1] == check
 
-
     @api.multi
     def button_check_isbn(self):
         for book in self:
@@ -36,3 +92,11 @@ class Book(models.Model):
             if book.isbn and not book._check_isbn():
                 raise Warning('%s is an invalid ISBN' % book.isbn)
             return True
+
+    @api.constrains('isbn')
+    def _constrain_isbn_valid(self):
+        for book in self:
+            if book.isbn and not book._check_isbn():
+                raise ValidationError('%s is an invalid ISBN' % book.isbn)
+
+
