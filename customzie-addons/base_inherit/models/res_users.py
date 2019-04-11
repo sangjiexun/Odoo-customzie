3a3c59bdd8571fa36a4c4ffc1e4901f31aa102fb
chenkaichang <chenkaichang.hit@gmail.com>
2019-04-11 14:07:41 +0900

    漏れたメソッドを追加

--- customzie-addons/base_inherit/models/res_users.py
+++ customzie-addons/base_inherit/models/res_users.py
@@ -57,3 +57,23 @@ class Users(models.Model):
                         _logger.exception("Failed to update web.base.url configuration parameter")
         return uid
 
+
+    @classmethod
+    def _login_by_user_barcode(cls, db, user_barcode):
+        ip = request.httprequest.environ['REMOTE_ADDR'] if request else 'n/a'
+        try:
+            with cls.pool.cursor() as cr:
+                self = api.Environment(cr, SUPERUSER_ID, {})[cls._name]
+                with self._assert_can_auth():
+                    user = self.search(self._get_user_barcode_domain(user_barcode))
+                    if not user:
+                        raise AccessDenied()
+                    user = user.sudo(user.id)
+                    user._update_last_login()
+        except AccessDenied:
+            _logger.info("Login failed for db:%s login:%s from %s", db, user_barcode, ip)
+            raise
+
+        _logger.info("Login successful for db:%s login:%s from %s", db, user_barcode, ip)
+
+        return user.id
