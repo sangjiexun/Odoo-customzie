a047d238751cef9b9af36c347667c80737086f12
許鵬 <47692777+alphaxp41@users.noreply.github.com>
2019-04-12 19:00:21 +0900

    Add files via upload

--- customzie-addons/after_service/__manifest__.py
+++ customzie-addons/after_service/__manifest__.py
@@ -9,12 +9,12 @@
     'application': True,
     'license': 'LGPL-3',
     'data': [
+        'security/after_service_security.xml',
+        'security/ir.model.access.csv',
         'views/after_service_views.xml',
         'views/after_service_templates.xml',
         'data/treatment_data.xml',
         'data/ir_sequence_data.xml',
-        'security/after_service_security.xml',
-        'security/ir.model.access.csv',
         'report/after_service_report.xml'
 
     ],
