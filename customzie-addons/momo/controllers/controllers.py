e75ec9d58d6746a949d946eba9d0a2855f0ce482
許鵬 <47692777+alphaxp41@users.noreply.github.com>
2019-05-24 15:14:32 +0900

    barcode pdf print

--- customzie-addons/momo/controllers/controllers.py
+++ customzie-addons/momo/controllers/controllers.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
-from odoo import http
-
+from odoo import http,_
+from odoo.http import request
 
 class Momo(http.Controller):
 
-    @http.route('/momo/momo/hello/', auth='public')
-    def hello(self, **kw):
-        return http.request.render('momo.barcode_scanner', {
-        })
+    @http.route('/momo/reports/<string:pdf_name>', auth='public')
+    def get_pdf_file(self, pdf_name):
+        pdf_path = "/home/developer/odoo-dev/odoo/customzie-addons/momo/reports/"
+        pdf_file = pdf_path + pdf_name
+        pdf = open(pdf_file,'rb')
+        pdfhttpheaders = [
+            ('Content-Type','application/pdf'),
+        ]
+        return request.make_response(pdf,headers=pdfhttpheaders)
