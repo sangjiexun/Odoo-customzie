95b00c906915112cf21baa3fbffac265fe69f3d4
Youshinan <youshinan82@gmail.com>
2019-05-30 01:26:25 +0900

    add company stamp

---
+++ customzie-addons/company_stamp/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Company Stamp",
+    'description': "Company Stamp",
+    'author': "youshinan",
+    'version': '12.0.1.0',
+    'category': '',
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+    'application': False,
+    'data': [
+        'views/res_company_views.xml',
+    ],
+}
---
+++ customzie-addons/company_stamp/models/res_company.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    company_stamp = fields.Binary("Company Stamp", attachment=True)
